46e781e315102c256d8b1f1bfdc0959059b180dc
tianzhao <tianzhao@tju.edu.cn>
2020-05-11 15:06:20 +0800

    add future predict

--- src/Model.py
+++ src/Model.py
@@ -10,6 +10,7 @@ from LSTM import LSTM
 
 
 class Model():
+
     def __init__(self, stride =5, input_size=6, output_size=4,learning_rate=0.00001,EPOCH = 1000):
         self.stride = stride
         self.input_size = input_size
@@ -20,7 +21,6 @@ class Model():
         self.X_MAX = [100, 16.5]
         self.Y_MIN = [360, 4, 1, 10]
         self.Y_MAX = [850, 12, 4, 28]
-
 
     # Traverses the specified directory to display all filenames under the directory.
     def eachFile(self,filepath):
@@ -49,6 +49,7 @@ class Model():
         fopen.close()
         return x,y
 
+    # 获取数据
     def getData(self,data_files_dic,normalized,percent):
         data_files_path = self.eachFile(data_files_dic)
 
@@ -89,8 +90,8 @@ class Model():
             train_data_y_.append(temp)
 
         for i in range(len(test_data_x)-self.stride):
-            temp_input = np.array(train_data_x[i:i + self.stride, 0:])
-            temp_output = np.array(train_data_y[i:i + self.stride, 0:])
+            temp_input = np.array(test_data_x[i:i + self.stride, 0:])
+            temp_output = np.array(test_data_y[i:i + self.stride, 0:])
             temp = np.concatenate((temp_input, temp_output), axis=1)
             temp = temp.flatten()
             test_data_x_.append(temp)
@@ -119,23 +120,29 @@ class Model():
 
     # Normalized data X
     def normalizedDataX(self, data_x):
-        for i in range(data_x.shape[1]):
+        data_x = np.array(data_x)
+        print(data_x.shape)
+        for i in range(2):
             # max_data_x = np.max(data_x[:, i])
             # data_x[:, i]/=max_data_x
             data_x[:, i] = (data_x[:, i] - self.X_MIN[i]) / (self.X_MAX[i] - self.X_MIN[i])
+
+        for i in range(4):
+            # max_data_x = np.max(data_x[:, i])
+            # data_x[:, i]/=max_data_x
+            data_x[:, 2+i] = (data_x[:, 2+i] - self.Y_MIN[i]) / (self.Y_MAX[i] - self.Y_MIN[i])
 
         return data_x
 
     # De-Normalized data Y
     def de_normalizedDataY(self,data_y):
-
+        data_y = np.array(data_y)
         for i in range(data_y.shape[1]):
             # max_data_y = np.max(data_y[:, i])
             # data_y[:, i] /= max_data_y
             data_y[:, i] = data_y[:, i] *(self.Y_MAX[i] - self.Y_MIN[i]) + self.Y_MIN[i]
 
         return data_y
-
 
     def train_Net(self,data_files_dic="./data/",normalized=False,percent=0.9):
         f = open("./log/train_Net_"+str(self.stride)+".txt","w")
@@ -245,28 +252,6 @@ class Model():
         f.write("\n"+"CO误差率: " + str(float(loss[3] * 100))[:8] + "%")
         print("平均耗时：", (time2 - time1) / len(testloader))
         f.write("\n"+"平均耗时："+str((time2 - time1) / len(testloader)))
-
-    def predict_Net(self, data_x, normalized=False):
-        print("-------------------------------------------")
-        if normalized:
-            data_x = self.normalizedDataX(data_x)
-
-        data_x = torch.Tensor(data_x)
-
-        # Load the Net_model
-        net = torch.load("./models/Net_model_"+str(self.stride)+".pth")
-
-        data_y = []
-        time1 = time.time()
-        for tx in data_x:
-            output = net(torch.unsqueeze(tx, dim=0))
-            output = output.detach().numpy()[0]
-            time2 = time.time()
-            data_y.append(output)
-        print("用时："+str(time2-time1))
-        data_y = np.array(data_y)
-        data_y = self.de_normalizedDataY(data_y)
-        return data_y
 
     def train_LSTM(self,data_files_dic = "./data/", normalized=False, percent=0.9):
         f = open("./log/train_LSTM_"+str(self.stride)+".txt","w")
@@ -383,12 +368,13 @@ class Model():
         print("平均耗时：", (time2 - time1) / len(testloader))
         f.write("\n"+"平均耗时："+str((time2 - time1) / len(testloader)))
 
-
-    def predict_LSTM(self, data_x, normalized=False):
-        print("-------------------------------------------")
+    def predict_LSTM(self, data_x, normalized=True, de_normalized=True):
+        # print("-------------------------------------------")
         if normalized:
             data_x = self.normalizedDataX(data_x)
 
+        data_x = np.array(data_x).flatten()
+        data_x = data_x.reshape([1, data_x.shape[0]])
         data_x = torch.Tensor(data_x)
 
         # Load the Net_model
@@ -402,17 +388,186 @@ class Model():
             output = output.detach().numpy()[0][0]
             time2 = time.time()
             data_y.append(output)
-        print("用时："+str(time2-time1))
+        # print("用时："+str(time2-time1))
         data_y = np.array(data_y)
-        data_y = self.de_normalizedDataY(data_y)
+        if de_normalized:
+            data_y = self.de_normalizedDataY(data_y)
         return data_y
+
+    def predict_Net(self, data_x, normalized=True,de_normalized=True):
+        # print("-------------------------------------------")
+        if normalized:
+            data_x = self.normalizedDataX(data_x)
+
+        data_x = np.array(data_x).flatten()
+        data_x = data_x.reshape([1,data_x.shape[0]])
+        data_x = torch.Tensor(data_x)
+
+        # Load the Net_model
+        net = torch.load("./models/Net_model_"+str(self.stride)+".pth")
+
+        data_y = []
+        time1 = time.time()
+        for tx in data_x:
+            output = net(torch.unsqueeze(tx, dim=0))
+            output = output.detach().numpy()[0]
+            time2 = time.time()
+            data_y.append(output)
+        # print("用时："+str(time2-time1))
+        data_y = np.array(data_y)
+        if de_normalized:
+            data_y = self.de_normalizedDataY(data_y)
+        return data_y
+
+    def test_future_LSTM(self, data_files_dic="./data/", normalized=False, percent=0.9, future=10):
+        f = open("./log/test_future_LSTM_" + str(future) + ".txt", "w")
+        print("-------------------------------------------")
+        f.write("\n" + "-------------------------------------------")
+        print("Future test LSTM Model...")
+        f.write("\n" + "Future test LSTM Model...")
+        # all trian data and test data files path
+        data_files_path = self.eachFile(data_files_dic)
+
+        # Get test data
+        data_x = []
+        data_y = []
+        for data_file in data_files_path:
+            data_x, data_y = self.readFile(data_file, data_x, data_y)
+        data_x = np.array(data_x)[:, 1:]
+        data_y = np.array(data_y)
+        if normalized:
+            data_x, data_y = self.normalizedData(data_x, data_y)
+
+        test_data_len = int((1 - percent) * len(data_x))
+        test_data_x = data_x[:test_data_len]
+        test_data_y = data_y[:test_data_len]
+        test_data_x = np.array(test_data_x)
+        test_data_y = np.array(test_data_y)
+
+        # Print the size of train data and test data
+        print("Test data shape:")
+        f.write("\n" + "Test data shape:")
+        print(test_data_x.shape)
+        f.write("\n" + str(test_data_x.shape))
+        print(test_data_y.shape)
+        f.write("\n" + str(test_data_y.shape))
+
+        loss = np.array([0, 0, 0, 0], float)
+        time1 = time.time()
+        for i in range(self.stride,len(test_data_y)-future+1):
+            # 需要不断的预测更新
+            tx_y = np.array(test_data_y[i - self.stride:i])
+            for j in range(i,i+future):
+                tx_x = np.array(test_data_x[j - self.stride:j])
+                # 更新 tx_y
+                if j != i:
+                    tx_y = tx_y[1:]
+                    tx_y = np.append(tx_y, predict_y, axis=0)
+                tx = np.concatenate((tx_x,tx_y),axis=1)
+                ty = np.array(test_data_y[j])
+                predict_y = self.predict_LSTM(tx, normalized=False,de_normalized=False)
+            predict_y = predict_y[0]
+            loss_ = np.abs(predict_y - ty) / ty
+            for i in range(len(loss_)):
+                loss[i] = loss[i] + loss_[i]
+            print(predict_y, ty)
+            f.write("\n" + str(predict_y) + " " + str(ty))
+        time2 = time.time()
+        loss /= (len(test_data_x)-self.stride-future+1)
+        print("LSTM Model Test:")
+        f.write("\n" + "LSTM Model Test:")
+        print("---------------------------------------------------")
+        f.write("\n" + "---------------------------------------------------")
+        print("气化温度误差率: " + str(float(loss[0] * 100))[:8] + "%")
+        f.write("\n" + "气化温度误差率: " + str(float(loss[0] * 100))[:8] + "%")
+        print("H2误差率: " + str(float(loss[1] * 100))[:8] + "%")
+        f.write("\n" + "H2误差率: " + str(float(loss[1] * 100))[:8] + "%")
+        print("CH4误差率: " + str(float(loss[2] * 100))[:8] + "%")
+        f.write("\n" + "CH4误差率: " + str(float(loss[2] * 100))[:8] + "%")
+        print("CO误差率: " + str(float(loss[3] * 100))[:8] + "%")
+        f.write("\n" + "CO误差率: " + str(float(loss[3] * 100))[:8] + "%")
+        print("平均耗时：", (time2 - time1) / (len(test_data_x)-self.stride-future+1))
+        f.write("\n" + "平均耗时：" + str((time2 - time1) / (len(test_data_x)-self.stride-future+1)))
+
+    def test_future_Net(self, data_files_dic="./data/", normalized=False, percent=0.9, future=10):
+        f = open("./log/test_future_Net_" + str(future) + ".txt", "w")
+        print("-------------------------------------------")
+        f.write("\n" + "-------------------------------------------")
+        print("Future test Net Model...")
+        f.write("\n" + "Future test Net Model...")
+        # all trian data and test data files path
+        data_files_path = self.eachFile(data_files_dic)
+
+        # Get test data
+        data_x = []
+        data_y = []
+        for data_file in data_files_path:
+            data_x, data_y = self.readFile(data_file, data_x, data_y)
+        data_x = np.array(data_x)[:, 1:]
+        data_y = np.array(data_y)
+        if normalized:
+            data_x, data_y = self.normalizedData(data_x, data_y)
+
+        test_data_len = int((1 - percent) * len(data_x))
+        test_data_x = data_x[:test_data_len]
+        test_data_y = data_y[:test_data_len]
+        test_data_x = np.array(test_data_x)
+        test_data_y = np.array(test_data_y)
+
+        # Print the size of train data and test data
+        print("Test data shape:")
+        f.write("\n" + "Test data shape:")
+        print(test_data_x.shape)
+        f.write("\n" + str(test_data_x.shape))
+        print(test_data_y.shape)
+        f.write("\n" + str(test_data_y.shape))
+
+        loss = np.array([0, 0, 0, 0], float)
+        time1 = time.time()
+        for i in range(self.stride,len(test_data_y)-future+1):
+            # 需要不断的预测更新
+            tx_y = np.array(test_data_y[i - self.stride:i])
+            for j in range(i,i+future):
+                tx_x = np.array(test_data_x[j - self.stride:j])
+                # 更新 tx_y
+                if j != i:
+                    tx_y = tx_y[1:]
+                    tx_y = np.append(tx_y, predict_y, axis=0)
+                tx = np.concatenate((tx_x,tx_y),axis=1)
+                ty = np.array(test_data_y[j])
+                predict_y = self.predict_LSTM(tx, normalized=False,de_normalized=False)
+            predict_y = predict_y[0]
+            loss_ = np.abs(predict_y - ty) / ty
+            for i in range(len(loss_)):
+                loss[i] = loss[i] + loss_[i]
+            print(predict_y, ty)
+            f.write("\n" + str(predict_y) + " " + str(ty))
+        time2 = time.time()
+        loss /= (len(test_data_x)-self.stride-future+1)
+        print("Net Model Test:")
+        f.write("\n" + "Net Model Test:")
+        print("---------------------------------------------------")
+        f.write("\n" + "---------------------------------------------------")
+        print("气化温度误差率: " + str(float(loss[0] * 100))[:8] + "%")
+        f.write("\n" + "气化温度误差率: " + str(float(loss[0] * 100))[:8] + "%")
+        print("H2误差率: " + str(float(loss[1] * 100))[:8] + "%")
+        f.write("\n" + "H2误差率: " + str(float(loss[1] * 100))[:8] + "%")
+        print("CH4误差率: " + str(float(loss[2] * 100))[:8] + "%")
+        f.write("\n" + "CH4误差率: " + str(float(loss[2] * 100))[:8] + "%")
+        print("CO误差率: " + str(float(loss[3] * 100))[:8] + "%")
+        f.write("\n" + "CO误差率: " + str(float(loss[3] * 100))[:8] + "%")
+        print("平均耗时：", (time2 - time1) / (len(test_data_x)-self.stride-future+1))
+        f.write("\n" + "平均耗时：" + str((time2 - time1) / (len(test_data_x)-self.stride-future+1)))
 
 
 if __name__ == '__main__':
-    for i in [5,10,15,20]:
+    for i in [5,10,15,20,25,30]:
         model = Model(stride=i,EPOCH=1000)
-        model.train_Net(normalized=True)
+        # model.train_Net(normalized=True)
         model.test_Net(normalized=True)
 
-        model.train_LSTM(normalized=True)
+        # model.train_LSTM(normalized=True)
         model.test_LSTM(normalized=True)
+    # model = Model(stride=10, EPOCH=1000)
+    # model.test_future_LSTM(normalized=True,future=10)
+    # model.test_future_Net(normalized=True,future=10)
